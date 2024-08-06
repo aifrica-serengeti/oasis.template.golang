320f496c8f2199791bb6a9e373b23c415664045d
aifrica <test@aifrica.co.kr>
2024-08-06 07:56:52 +0000

    template init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/knative/docs/code-samples/serving/hello-world/helloworld-go
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.8.1
---
+++ helloworld.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a Gin router
+	router := gin.Default()
+
+	// Define the handler function
+	router.GET("/", func(c *gin.Context) {
+		log.Print("helloworld: received a request")
+		target := os.Getenv("TARGET")
+		if target == "" {
+			target = "World"
+		}
+		c.String(200, fmt.Sprintf("Hello %s!\n", target))
+	})
+
+	// Determine the port to listen on
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("helloworld: listening on port %s", port)
+
+	// Start the Gin server
+	log.Fatal(router.Run(fmt.Sprintf(":%s", port)))
+}
